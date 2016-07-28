d18a242d56a002572ef8094b66aad6251d615874
Beeno Tung <aabbcc1241@yahoo.com.hk>
2016-07-28 20:28:32 +0800

    changed to seed format

--- src/index.js
+++
@@ -1,3 +0,0 @@
-console.log("hello");
-[1, 2, 3].map(x=>console.log(x));
-console.log('world');
---
+++ src/package.ts
@@ -0,0 +1,5 @@
+///<reference path="../typings/tsd.d.ts"/>
+
+///<reference path="../lib/jslib/dist/horizon-param.d.ts"/>
+///<reference path="../lib/jslib/dist/stub.d.ts"/>
+///<reference path="../lib/jslib/dist/utils.d.ts"/>
